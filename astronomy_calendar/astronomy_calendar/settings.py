b67085baadb68fc6fe9a6ef4425f8445f60898e8
Ralfmat <mateuszwanda@outlook.com>
2023-11-15 18:24:22 +0100

    mati

--- astronomy_calendar/astronomy_calendar/settings.py
+++ astronomy_calendar/astronomy_calendar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i7$wim_m4=!-ncihnlg$b2rnbb4-z#r1+z2!685_((eqfa2jw8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
